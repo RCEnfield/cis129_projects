b35d2e6cf8e1fa3f8177e1a4837cc1c398a80e03
RCEnfield <158790792+RCEnfield@users.noreply.github.com>
2024-02-04 04:06:55 -0700

    Typo Fix cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -5,4 +5,4 @@ print('There once were four sister-wife spiders named Eeny, Meeny, Miney, and Mo
 print('They were married to Tiger, the largest spider in the town, who brought misfortune to all around him.')
 print('Eeny, Meeny, Miney, and Mo decided to arrange a plan to rid the people of their terrible spouce once and for all.')
 print('After coming home from terrorizing the town, Tiger had all eight limbs pinned down by the sister wives as they shouted their names one after another.')
-print('"Eeny," "Meeny," "Miney," "Mo!" The spider sisters popped off each limb from Tiger and saved the town—becoming black widows in the process.")
+print('"Eeny," "Meeny," "Miney," "Mo!" The spider sisters popped off each limb from Tiger and saved the town—becoming black widows in the process.')
