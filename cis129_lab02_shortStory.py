b6147c136c8259971068769e4ade64e06abb6bef
RCEnfield <158790792+RCEnfield@users.noreply.github.com>
2024-02-04 04:04:53 -0700

    Add Story cis129_lab02_shortStory.py
    Deletes 'Hello, World' text, and replaces it with a short story about sister-wife spiders becoming black widows.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Reece Enfield
+# Prints a short story
+# Replaced 'Hello, World!' statement with a short story
+print('There once were four sister-wife spiders named Eeny, Meeny, Miney, and Mo.')
+print('They were married to Tiger, the largest spider in the town, who brought misfortune to all around him.')
+print('Eeny, Meeny, Miney, and Mo decided to arrange a plan to rid the people of their terrible spouce once and for all.')
+print('After coming home from terrorizing the town, Tiger had all eight limbs pinned down by the sister wives as they shouted their names one after another.')
+print('"Eeny," "Meeny," "Miney," "Mo!" The spider sisters popped off each limb from Tiger and saved the town—becoming black widows in the process.")
