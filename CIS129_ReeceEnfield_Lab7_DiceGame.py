407b007636f41fde1c4bb19ab5ee6921d991d2fc
RCEnfield <158790792+RCEnfield@users.noreply.github.com>
2024-03-11 17:03:42 -0700

    Add files via upload

---
+++ CIS129_ReeceEnfield_Lab7_DiceGame.py
@@ -0,0 +1,64 @@
+# CIS129_ReeceEnfield_Lab7_DiceGame.py
+# Reece Enfield
+# March 11, 2024
+# This program is a playable random dice game
+
+# Lab 7-3 The Dice Game
+# add libraries needed
+
+import random
+
+# the main function
+def main():
+    print()
+
+    # initialize variables
+    endProgram = 'no'
+    playerOne = 'NO NAME'
+    playerTwo = 'NO NAME'
+
+    # call to inputNames
+    playerOne, playerTwo = inputNames(playerOne, playerTwo)
+
+    # while loop to run program again
+    while endProgram == 'no':
+
+        # populate variables
+        winnerName = 'NO NAME'
+        p1number = 0
+        p2number = 0
+
+        # call to rollDice
+        winnerName = rollDice(p1number, p2number, playerOne, playerTwo, winnerName)
+
+        # call to displayInfo
+        displayInfo(winnerName)
+
+        endProgram = input('Do you want to end program? (yes/no): ')
+
+#this function gets the players names
+def inputNames(playerOne, playerTwo):
+    playerOne = input("Enter player one's name: ")
+    playerTwo = input("Enter player two's name: ")
+    return playerOne, playerTwo
+
+#this function will get the random values
+def rollDice(p1number, p2number, playerOne, playerTwo, winnerName):
+    p1number = random.randint(1, 6)
+    p2number = random.randint(1, 6)
+
+    if p1number == p2number:
+        winnerName = "TIE"
+    elif p1number > p2number:
+        winnerName = playerOne
+    elif p1number < p2number:
+        winnerName = playerTwo
+    
+    return winnerName
+
+#this function displays the winner
+def displayInfo(winnerName):
+    print(winnerName, "wins!")
+
+# calls main
+main()
